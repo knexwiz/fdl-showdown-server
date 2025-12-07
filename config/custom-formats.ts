1f6ab48b51aed9647602e48b2197e4272dbe97e5
Spriing <knex12wiz@gmail.com>
2025-12-07 14:29:53 -0700

    Create custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,22 @@
+export const Formats: FormatList = [
+
+		{
+        	section: "FDL Custom",
+    	},
+    	{
+        	name: "[Gen 9] Modern: Gen V Doubles",
+        	mod: 'gen9fdl5',
+        	gameType: 'doubles',
+        	bestOfDefault: true,
+        	ruleset: ['!! Adjust Level = 50', 'Terastal Clause'],
+			unbanlist: ['Mythical'],
+    	},
+    	{
+        	name: "[Gen 9] Modern: Gen V",
+        	mod: 'gen9fdl5',
+        	bestOfDefault: true,
+        	ruleset: [ 'Terastal Clause'],
+			unbanlist: ['Mythical'],
+    	},
+
+];
